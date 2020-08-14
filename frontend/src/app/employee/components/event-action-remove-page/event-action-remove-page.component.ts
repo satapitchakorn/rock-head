146f09a7574c43400380e13b4faf357fdc19c7c4
Hasaneeya Kaenthram <hasaneeyakae@mathregit.group>
2020-08-14 11:14:39 +0700

    [Edited] Edit code for create Remove employee in EventActionRemovePage

--- frontend/src/app/employee/components/event-action-remove-page/event-action-remove-page.component.ts
+++ frontend/src/app/employee/components/event-action-remove-page/event-action-remove-page.component.ts
@@ -10,6 +10,22 @@ export class EventActionRemovePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    //Check Valid forms 
+    (function () {
+      'use strict';
+      window.addEventListener('load', function () {
+        var forms = document.getElementsByClassName('needs-validation');
+        var validation = Array.prototype.filter.call(forms, function (form) {
+          form.addEventListener('submit', function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          }, false);
+        });
+      }, false);
+    })();
   }
 
 }
