2b70582d25908f5c809d3455b0e9bd2ee2e8e20d
Narongrit Rodphroboon <narongrit.rodphroboon@allianz.com>
2020-08-14 09:20:40 +0700

    [Added]: EventActionAddPage form and log on submit.

--- frontend/src/app/employee/components/event-action-add-page/event-action-add-page.component.ts
+++ frontend/src/app/employee/components/event-action-add-page/event-action-add-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-event-action-add-page',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-action-add-page.component.css']
 })
 export class EventActionAddPageComponent implements OnInit {
+  dataForm = new FormGroup({
+    passport: new FormControl(''),
+    employee_no: new FormControl(''),
+    firstname: new FormControl(''),
+    lastname: new FormControl(''),
+    position: new FormControl(''),
+    start_date: new FormControl(''),
+    email: new FormControl(''),
+    phone: new FormControl('')
+  });
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+    console.log(this.dataForm.value);
+  }
 }
