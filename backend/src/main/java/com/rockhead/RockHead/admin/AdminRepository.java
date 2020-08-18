9b1c857bd35befaba0fd92313ac4383baddc3068
Kanawat Phuengphadung <kanawatphu@mathregit.group>
2020-08-18 14:56:03 +0700

    [Edited]: Delete method findOneByAdminNo in AdminRepository.java .

--- backend/src/main/java/com/rockhead/RockHead/admin/AdminRepository.java
+++ backend/src/main/java/com/rockhead/RockHead/admin/AdminRepository.java
@@ -3,5 +3,4 @@ package com.rockhead.RockHead.admin;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface AdminRepository extends MongoRepository<Admin, String> {
-    Admin findOneByAdminNo(int no);
 }
